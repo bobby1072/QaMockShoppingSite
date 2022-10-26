31618a1f7a5ffd8909b2f27d2e0c347d95cd0868
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-26 14:28:39 +0100

    Program complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
 import prodReq, { Iproduct } from "./Requests/ProductRequest";
 import ProductBoxMap from "./ProductBox";
+import BasketDisplay from "./BasketComp";
 function App(): JSX.Element {
   const [searchButtonLoading, setSearchButtonLoading] =
     React.useState<boolean>(false);
@@ -11,53 +12,70 @@ function App(): JSX.Element {
     document.title = "Product Finder";
   }, [products]);
   return (
-    <div className="topLevelDiv">
-      <div className="MainBox">
-        <LoadingButton
-          sx={{ width: 100, mb: 2 }}
-          onClick={async () => {
-            setSearchButtonLoading(true);
-            let uniqueNums: boolean = false;
-            let randProdNum1: number = 0;
-            let randProdNum2: number = 0;
-            let randProdNum3: number = 0;
-            while (!uniqueNums) {
-              randProdNum1 = Math.floor(Math.random() * 20);
-              randProdNum2 = Math.floor(Math.random() * 20);
-              randProdNum3 = Math.floor(Math.random() * 20);
-              if (
-                !(
-                  randProdNum3 === randProdNum2 ||
-                  randProdNum1 === randProdNum2 ||
-                  randProdNum1 === randProdNum3
-                ) &&
-                randProdNum1 !== 0 &&
-                randProdNum2 !== 0 &&
-                randProdNum3 !== 0
-              ) {
-                uniqueNums = true;
+    <div>
+      <div className="topLevelDiv">
+        <div className="MainBox">
+          <LoadingButton
+            sx={{ width: 100, mb: 2 }}
+            onClick={async () => {
+              setSearchButtonLoading(true);
+              let uniqueNums: boolean = false;
+              let randProdNum1: number = 0;
+              let randProdNum2: number = 0;
+              let randProdNum3: number = 0;
+              while (!uniqueNums) {
+                randProdNum1 = Math.floor(Math.random() * 20);
+                randProdNum2 = Math.floor(Math.random() * 20);
+                randProdNum3 = Math.floor(Math.random() * 20);
+                if (
+                  !(
+                    randProdNum3 === randProdNum2 ||
+                    randProdNum1 === randProdNum2 ||
+                    randProdNum1 === randProdNum3
+                  ) &&
+                  randProdNum1 !== 0 &&
+                  randProdNum2 !== 0 &&
+                  randProdNum3 !== 0
+                ) {
+                  uniqueNums = true;
+                }
               }
-            }
-            const prod1 = randProdNum1 && (await prodReq(randProdNum1));
-            const prod2 = randProdNum2 && (await prodReq(randProdNum2));
-            const prod3 = randProdNum3 && (await prodReq(randProdNum3));
-            prod1 && prod2 && prod3 && setProducts([prod1, prod2, prod3]);
-            setSearchButtonLoading(false);
-          }}
-          loading={searchButtonLoading}
-          variant="contained"
-        >
-          Search random products
-        </LoadingButton>
-        {products && (
-          <ProductBoxMap
-            ProductList={products}
-            addItemToBask={(item) => {
-              setBasket(basket.concat([item]));
+              const prod1 = randProdNum1 && (await prodReq(randProdNum1));
+              const prod2 = randProdNum2 && (await prodReq(randProdNum2));
+              const prod3 = randProdNum3 && (await prodReq(randProdNum3));
+              prod1 && prod2 && prod3 && setProducts([prod1, prod2, prod3]);
+              setSearchButtonLoading(false);
             }}
-          />
-        )}
+            loading={searchButtonLoading}
+            variant="contained"
+          >
+            Search random products
+          </LoadingButton>
+          {products && (
+            <ProductBoxMap
+              ProductList={products}
+              addItemToBask={(item) => {
+                const makeSureUnique = basket.find((element) => {
+                  return element.Id === item.Id;
+                });
+                !makeSureUnique && setBasket(basket.concat([item]));
+              }}
+            />
+          )}
+        </div>
       </div>
+      {basket && (
+        <BasketDisplay
+          Items={basket}
+          removeItemFromBasket={(item) => {
+            setBasket(
+              basket.filter((element) => {
+                return element.Id !== item.Id;
+              })
+            );
+          }}
+        />
+      )}
     </div>
   );
 }
--- src/BasketComp.tsx
+++ src/BasketComp.tsx
@@ -1,4 +1,77 @@
-function BasketDisplay(): JSX.Element {
-  return <div></div>;
+import { Iproduct } from "./Requests/ProductRequest";
+import { Box, ThemeProvider, createTheme } from "@mui/system";
+import DeleteIcon from "@mui/icons-material/Delete";
+interface IBasketProps {
+  Items: Iproduct[];
+  removeItemFromBasket: (item: Iproduct) => void;
+}
+interface IBasketItemProps {
+  SingleItem: Iproduct;
+  removeItemFromBasket: (item: Iproduct) => void;
+}
+const theme = createTheme({
+  palette: {
+    background: {
+      paper: "#fff",
+    },
+    text: {
+      primary: "#173A5E",
+      secondary: "#46505A",
+    },
+    action: {
+      active: "#001E3C",
+    },
+    success: {
+      dark: "#009688",
+    },
+  },
+});
+function BasketItem(props: IBasketItemProps): JSX.Element {
+  const myItem = props.SingleItem;
+  return (
+    <div className="BasketItem">
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            width: 500,
+            bgcolor: "background.paper",
+            boxShadow: 1,
+            borderRadius: 2,
+            p: 2,
+            minWidth: 300,
+          }}
+        >
+          <Box sx={{ color: "text.secondary" }}>
+            {`Price: £${myItem.Price}`}
+          </Box>
+          <Box
+            sx={{ color: "text.primary", fontSize: 24, fontWeight: "medium" }}
+          >
+            {myItem.Title}
+          </Box>
+        </Box>
+      </ThemeProvider>
+      <DeleteIcon
+        onClick={() => {
+          props.removeItemFromBasket(myItem);
+        }}
+      />
+    </div>
+  );
+}
+function BasketDisplay(props: IBasketProps): JSX.Element {
+  const item = props.Items;
+  return (
+    <div className="topLevelDiv">
+      <div className="BasketBox">
+        {item.map((element) => (
+          <BasketItem
+            SingleItem={element}
+            removeItemFromBasket={props.removeItemFromBasket}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default BasketDisplay;
